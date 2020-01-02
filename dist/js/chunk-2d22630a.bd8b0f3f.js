feb433dff1717bd721c1fc03bc4796f208125bfb
liaoliao <1030219089a@gmail.com>
2020-01-02 10:32:10 +0800

    update web

---
+++ dist/js/chunk-2d22630a.bd8b0f3f.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22630a"],{e834:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h2",[n._v("\n  gql-superadmin\n")])},l=[],a=t("2877"),c={},r=Object(a["a"])(c,u,l,!1,null,null,null);e["default"]=r.exports}}]);
