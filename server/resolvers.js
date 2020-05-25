9986ac20bdf2da0d5185ac633d7dddb470af27a6
liaoliao <1030219089a@gmail.com>
2020-05-25 21:44:25 +0800

    Feat: 解析防止xss攻击

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,12 @@
 
-import Crypto from 'crypto'
+import {createHash} from 'crypto'
 import {generate} from 'shortid'
+import {JSDOM} from 'jsdom'
+import DOMPurify  from 'dompurify'
+
+// 防xss攻击
+const {window} = new JSDOM('<!DOCTYPE html>')
+const domPurify = DOMPurify(window)
 
 const PASSWORD_MD5 = 'f40a80dc6e7aa63c2ade7f06eee4c088'
 
@@ -28,11 +34,11 @@ const resolvers = {
   Mutation: {
     addArticle: (root, {input}, {db}) => {
       // 验证密码
-      if (Crypto.createHash('md5').update(input.password).update("liaoliaojun").digest('hex') !== PASSWORD_MD5) return 0
+      if (createHash('md5').update(input.password).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
       const articleInfo = {
         article_id: generate(),
         article_title: input.article_title,
-        article_content: input.article_content,
+        article_content: domPurify.sanitize(input.article_content),
         article_views: 0,
         article_date: '2020.05.21',
         article_like_count: 0,
@@ -44,13 +50,13 @@ const resolvers = {
 
     updateArticle: (root, {input}, {db}) => {
       // 验证密码
-      if (Crypto.createHash('md5').update(input.password).update("liaoliaojun").digest('hex') !== PASSWORD_MD5) return 0
+      if (createHash('md5').update(input.password).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
       // 验证文章是否存在
       if (!db.get('articles').find({article_id: input.article_id}).value()) return 0
 
       db.get('articles').find({article_id: input.article_id}).assign({
         article_title: input.article_title,
-        article_content: input.article_content,
+        article_content: domPurify.sanitize(input.article_content),
       }).write()
       return input.article_id || 0
     },
