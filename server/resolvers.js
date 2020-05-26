b1847ea4950eec0c5181b54ee950a40e6017997f
liaoliao <1030219089a@gmail.com>
2020-05-26 17:55:23 +0800

    Feat: 添加字段marked原文 记录访客信息

--- server/resolvers.js
+++ server/resolvers.js
@@ -19,25 +19,28 @@ const resolvers = {
       return db.get('articles').value()
     },
     article: (root, {article_id}, {db, ip}) => {
-      const visitor = db.get('visitors').find({ip}).value()
-      const date = new Date()
-
-      if (!visitor) {
-        db.get('visitors').push({ip, visit_time_local: date.toLocaleString(), visit_time_stamp: date.getTime()}).write()
-      } else {
-        db.get('visitors').find({ip}).assign({visit_time_local: date.toLocaleString(), visit_time_stamp: date.getTime()}).write()
-      }
       return db.get('articles').find({article_id}).value()
     },
   },
 
   Mutation: {
+    setVisitor: (root, args, {db, ip}) => {
+      const visitor = db.get('visitors').find({ip}).value()
+      const date = new Date()
+      if (!visitor) {
+        db.get('visitors').push({ip, visit_time_local: date.toLocaleString('zh-CN'), visit_time_stamp: date.getTime()}).write()
+      } else {
+        db.get('visitors').find({ip}).assign({visit_time_local: date.toLocaleString('zh-CN'), visit_time_stamp: date.getTime()}).write()
+      }
+      return true
+    },
     addArticle: (root, {input}, {db}) => {
       // 验证密码
-      if (createHash('md5').update(input.password).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
+      if (createHash('md5').update(input.key).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
       const articleInfo = {
         article_id: generate(),
         article_title: input.article_title,
+        article_marked_content: input.article_marked_content,
         article_content: domPurify.sanitize(input.article_content),
         article_views: 0,
         article_date: '2020.05.21',
@@ -50,12 +53,13 @@ const resolvers = {
 
     updateArticle: (root, {input}, {db}) => {
       // 验证密码
-      if (createHash('md5').update(input.password).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
+      if (createHash('md5').update(input.key).update('liaoliaojun').digest('hex') !== PASSWORD_MD5) return 0
       // 验证文章是否存在
       if (!db.get('articles').find({article_id: input.article_id}).value()) return 0
 
       db.get('articles').find({article_id: input.article_id}).assign({
         article_title: input.article_title,
+        article_marked_content: input.article_marked_content,
         article_content: domPurify.sanitize(input.article_content),
       }).write()
       return input.article_id || 0
